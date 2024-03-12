266766a02ac520f968941e47ef919826b3b5dc7a
mahtobhola431 <72021931+mahtobhola431@users.noreply.github.com>
2024-03-12 13:58:03 +0530

    react and tailwind started

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+
+      <div>
+      <h1 className="text-3xl font-bold underline bg-red-500">
+      Hello world!
+    </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
